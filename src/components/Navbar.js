ca3ca9005137f02d7c1da57617e43c6557627c79
eiron <eiron@ussi.com.ph>
2019-02-25 14:22:50 +0800

    about page routed to md file for cms

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ const Header = ({ siteTitle }) => (
         <div class="navbar-collapse collapse">
             <nav role="navigation">
                 <ul id="menu-main-menu" class="menu nav navbar-nav navbar-right pull-right">
-                    <li><a href="/">About</a></li>
+                    <li><a href="/about">About</a></li>
                     <li><a href="/">My videos</a></li>
                     <li class="last">   
                         Who's there? |                     
@@ -33,36 +33,8 @@ const Header = ({ siteTitle }) => (
                 </ul>
             </nav>
         </div>
-
-        <div class="mobile-menu">
-            <div class="menu-main mobile-menu-wrap">
-                <ul class="menu nav">
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/myvideos">My videos</a></li>
-                    <li>
-                      <a href="/myvideos">Login</a>
-                    </li>
-                </ul> 
-            </div>
-            <div class="menu-global mobile-menu-wrap">
-                <ul class="menu nav">
-                    <li class="first"><a href="http://www.sparkol.com">Sparkol</a></li>
-                    <li class="last"><a href="http://www.sparkol.com/legal/">Privacy and cookies</a></li>
-                </ul>
-            </div>
-            <div class="social mobile-menu-wrap">
-                <ul class="menu nav">
-                    <li><a href="https://twitter.com/SparkolHQ" class="icon-social icon-twitter" target="_blank">Twitter</a></li>
-                    <li><a href="https://www.facebook.com/SparkolHQ" class="icon-social icon-facebook" target="_blank">Facebook</a></li>
-                    <li><a href="https://www.youtube.com/channel/UC-M3N1dBICkLMU8fTlmpgjw" class="icon-social icon-youtube" target="_blank">YouTube</a></li>
-                    <li><a href="https://plus.google.com/u/0/+Sparkol/posts" class="icon-social icon-gplus" target="_blank">Google+</a></li>
-                    <li><a href="https://www.linkedin.com/company/2755634" class="icon-social icon-linkedin" target="_blank">LinkedIn</a></li>
-                    <li><a href="https://instagram.com/sparkolhq/" class="icon-social icon-instagram" target="_blank">Instagram</a></li>
-                </ul>
-            </div>
             {/* {{> slanted_edge}} */}
         </div>
-    </div>
     <SlantedEndge/>
 </header>
 )
