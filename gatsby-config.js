d843b71eadf7188d035173397980cf523b63f58c
Gelo Ilagan <gelo@ussi.com.ph>
2019-02-27 05:58:20 +0800

    add env variables, updated login link on navbar based on env

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,11 @@
+let activeEnv = process.env.ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+console.log(`Using environment config: '${activeEnv}'`)
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: 'sho.co for Sparkol',
