aa7ac52727e3470cf9fc23d90f9b393fab25c42d
Gelo Ilagan <gelo@ussi.com.ph>
2019-02-26 16:23:48 +0800

    added shoco.navbar, footer, slanted edge with styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,101 +1,100 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
-import logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
 
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-              <div className="columns">
-                <div className="column is-4">
-                <section className="menu">
-                    <ul className="menu-list">
-                      <li><Link to="/" className="navbar-item">Home</Link></li>
-                      <li><Link className="navbar-item" to="/about">About</Link></li>
-                      <li><Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                    <Link className="navbar-item" to="/contact/examples">
-                      Form Examples
-                    </Link>
-                    </li>
-                    <li><a
-                      className="navbar-item"
-                      href="/admin/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Admin
-                    </a></li>
-                    </ul>
-                  </section>
-                </div>
-                <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                  <li>
-                    <Link className="navbar-item" to="/blog">
-                      Latest Stories
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="navbar-item" to="/contact">
-                      Contact
-                    </Link>
-                  </li>
-                  </ul>
-                </section>
-                </div>
-                <div className="column is-4 social">
-                
-                  <a title="facebook" href="https://facebook.com">
-                    <img 
-                      src={facebook}
-                      alt="Facebook"
-                      style={{ width: '1em', height: '1em' }}
-                    />
+      <footer>
+        <div className="subscription">
+          <div className="container">
+            <a href="http://www.sparkol.com" title="Sparkol">
+              <span className="logo-link logo-sparkol-large">Sparkol</span>
+            </a>
+            <div className="subscription-wrap">
+              <ul className="products list-unstyled list-inline">
+                <li>
+                  <a className="logo-link logo-vs-large" href="http://www.videoscribe.co">
+                    <span className="product-name">VideoScribe</span>
                   </a>
-                  <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                  src={instagram}
-                  alt="Instagram"
-                  style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                  src={vimeo}
-                  alt="Vimeo"
-                  style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                </div>
-              </div>
+                  <span className="product-desc">Create your own whiteboard videos, fast</span>
+                </li>
+                <li>
+                  <a className="logo-link logo-tawe-large" href="http://www.tawe.co/">
+                    <span className="product-name">Tawe</span>
+                  </a>
+                  <span className="product-desc">Transform visual ideas into video instantly</span>
+                </li>
+              </ul>
+              <a href="https://my.sparkol.com/buy">Get a Sparkol subscription</a>
+              <p className="product-desc">And enjoy 2 video apps for the price of 1</p>
             </div>
+          </div>
+        </div>
+
+        <div className="sparkol-products">
+          <div className="container">
+              <ul className="menu nav">
+                <li>
+                <a href="http://www.videoscribe.co/anywhere" className="product-logo logo-vsa-small">
+                  <span className="product-name">VS Anywhere</span>
+                </a>
+                <span className="product-desc">VideoScribe for mobile</span>
+                </li>
+              <li>
+                <a href="http://www.sparkol.com/engage/" className="product-logo logo-blog-small">
+                  <span className="product-name">Sparkol blog</span>
+                </a>
+                <span className="product-desc">Let's get engaging</span>
+              </li>
+              <li>
+                <a href="http://www.sparkol.com/books" className="product-logo logo-ebook-small">
+                  <span className="product-name">Sparkol books</span>
+                </a>
+                <span className="product-desc">Free, inspiring ebooks</span>
+              </li>
+              <li>
+                <a href="http://www.animole.com" className="product-logo logo-animole-small">
+                  <span className="product-name">Animole</span>
+                </a>
+                <span className="product-desc">Find pro animators</span>
+              </li>
+              <li>
+                <a href="https://itunes.apple.com/app/tawe/id993448214" className="product-logo logo-tawe-small">
+                  <span className="product-name">Tawe</span>
+                </a>
+                <span className="product-desc">Tawe for mobile</span>
+              </li>
+              <li>
+                <a href="http://www.svgstudio.com/" className="product-logo logo-svg-small">
+                  <span className="product-name">SVG Studio</span>
+                </a>
+                <span className="product-desc">Royalty-free images</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="social">
+          <div className="container">
+            <ul className="menu">
+              <li><a href="https://twitter.com/SparkolHQ" className="icon-social icon-twitter" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+              <li><a href="https://www.facebook.com/SparkolHQ" className="icon-social icon-facebook" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+              <li><a href="https://www.youtube.com/channel/UC-M3N1dBICkLMU8fTlmpgjw" className="icon-social icon-youtube" target="_blank" rel="noopener noreferrer">YouTube</a></li>
+              <li><a href="https://plus.google.com/u/0/+Sparkol/posts" className="icon-social icon-gplus" target="_blank" rel="noopener noreferrer">Google+</a></li>
+              <li><a href="https://www.linkedin.com/company/2755634" className="icon-social icon-linkedin" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+              <li><a href="https://instagram.com/sparkolhq/" className="icon-social icon-instagram" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="copyright">
+          <div className="container">
+            <p className="small"><a href="https://my.sparkol.com" target="_blank" rel="noopener noreferrer">Sparkol Account</a></p>
+            <ul>
+              <li className="small">© 2018 Sparkol Limited, registered in England and Wales, no 06762963</li>
+              <li className="small">1 Temple Way 5th Floor, Bristol, BS2 0BY, UK</li>
+            </ul>
+            <p className="small"><a href="http://www.sparkol.com/legal/" target="_blank" rel="noopener noreferrer">Privacy and cookies</a></p>
+          </div>
         </div>
       </footer>
     )
---
+++ src/components/slantedEdge.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const SlantedEdge = ({ }) => (
+    <span className="icwrap">
+        <span className="ic1"></span>
+        <span className="ic2"></span>
+        <span className="ic3"></span>
+    </span>
+)
+
+export default SlantedEdge
