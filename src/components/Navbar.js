46630188db8be5b6e4d68c4899aa434ee91fbd90
Babyboy <dmcilagan@gmail.com>
2019-03-22 16:55:49 +0800

    added auth function with cognito (#3)
    New
    *add aws-amplify and aws-amplify-react
    *Using states, navbar is now dynamic with Authentication of the user
    *Authenticator by aws-amplify-react manages our login/sign up pages
    *add config files of aws config

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,52 +1,121 @@
 import PropTypes from "prop-types"
-import React from "react"
+import React, { Component } from "react"
 import shocologo from "../images/shoco_logo.png"
 import SlantedEndge from '../components/slantedEdge'
-import { Link } from 'gatsby'
+import { Link, navigate } from 'gatsby'
+import { Auth } from 'aws-amplify';
+
+
 
 import "./styles/header.scss"
 
-const Header = ({ siteTitle }) => (
-  <header id="header" className="navbar navbar-default">
-    <div className="container">
-        <div className="navbar-header">
-            <Link to="/">
-                <div className="logo navbar-btn pull-left" title="sho.co" rel="home">
-                    <img src={shocologo} alt="sho.co logo"/>
-                </div>
-            </Link>
+  class Header extends Component {
 
-            <button type="button" className="navbar-toggle">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-            </button>
-        </div>
+    constructor(props) {
+        super(props);
+        this.state = {
+          authState: 'loading',
+          user: ""
+        };
+        this.signOut = this.signOut.bind(this);
+      }
 
-        <div className="navbar-collapse collapse">
-            <nav role="navigation">
-                <ul id="menu-main-menu" className="menu nav navbar-nav navbar-right pull-right">
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/">My videos</Link></li>
-                    <li className="last">   
-                        Who's there? |                     
-                       <Link to="/">Login</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </div>
-    <SlantedEndge/>
-</header>
-)
+    componentWillMount() {
+        Auth.currentAuthenticatedUser({
+            bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+        }).then(user => {
+            console.log(user);
+            this.setState({authState: 'signedIn'});
+            this.setState({user: user.username});
+        })
+        .catch(err => {
+            console.log(err);
+            this.setState({authState: 'signIn'});  
+        });
+    }
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+    signOut() {
+        navigate("/");
+        this.setState({authState: 'signIn'});  
+        Auth.signOut().then(() => {
+        }).catch(e => {
+          console.log(e);
+        });
+      }
+
+    render() {
+        if(this.state.authState == 'signedIn') 
+            return (
+            <header id="header" className="navbar navbar-default">
+                    <div className="container">
+                        <div className="navbar-header">
+                            <Link to="/">
+                                <div className="logo navbar-btn pull-left" title="sho.co" rel="home">
+                                    <img src={shocologo} alt="sho.co logo"/>
+                                </div>
+                            </Link>
+            
+                            <button type="button" className="navbar-toggle">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                        </div>
+            
+                        <div className="navbar-collapse collapse">
+                            <nav role="navigation">
+                                <ul id="menu-main-menu" className="menu nav navbar-nav navbar-right pull-right">
+                                    <li><Link to="/about">About</Link></li>
+                                    <li><Link to="/">My videos</Link></li>
+                                    <li className="last">   
+                                        Yo, {this.state.user} |                     
+                                        <span onClick={this.signOut}>Logout</span>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <SlantedEndge/>
+                </header>
+            );
+        else 
+            return (
+                <header id="header" className="navbar navbar-default">
+                    <div className="container">
+                        <div className="navbar-header">
+                            <Link to="/">
+                                <div className="logo navbar-btn pull-left" title="sho.co" rel="home">
+                                    <img src={shocologo} alt="sho.co logo"/>
+                                </div>
+                            </Link>
+            
+                            <button type="button" className="navbar-toggle">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                        </div>
+            
+                        <div className="navbar-collapse collapse">
+                            <nav role="navigation">
+                                <ul id="menu-main-menu" className="menu nav navbar-nav navbar-right pull-right">
+                                    <li><Link to="/about">About</Link></li>
+                                    <li><Link to="/">My videos</Link></li>
+                                    <li className="last">   
+                                        Who's there? |                     
+                                        <Link to="/app">Login</Link>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <SlantedEndge/>
+                </header>
+            );
+        
+    }
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+export default Header;
--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -2,9 +2,17 @@ import React from "react"
 import { Router } from "@reach/router" // comes with gatsby v2
 import IndexPageTemplate from "../../templates/index-page"
 import PrivateRoute from "./component/PrivateRoute"
+import { withAuthenticator } from 'aws-amplify-react';
+import Amplify from 'aws-amplify';
+// Get the aws resources configuration parameters
+import aws_exports from '../../aws-exports'; // if you are using Amplify CLI
+import { navigate } from 'gatsby';
+
+Amplify.configure(aws_exports);
 
 // remember everything in /app/* is dynamic now!
 const App = () => {
+  navigate('/');
   return (
       <Router>
         <PrivateRoute path="/app/about" component={AboutPage} />
@@ -23,4 +31,4 @@ function PublicRoute(props) {
   return <div>{props.children}</div>
 }
 
-export default App
+export default withAuthenticator(App);
