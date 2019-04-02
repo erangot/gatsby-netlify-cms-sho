5a3e4d19a38fe26c79c644b710c8c13369427bed
Babyboy <dmcilagan@gmail.com>
2019-04-02 14:23:13 +0800

    customize labels for aws cognito login (#9)

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -3,12 +3,24 @@ import { Router } from "@reach/router" // comes with gatsby v2
 import IndexPageTemplate from "../../templates/index-page"
 import PrivateRoute from "./component/PrivateRoute"
 import { withAuthenticator } from 'aws-amplify-react';
-import Amplify from 'aws-amplify';
+import Amplify, { I18n } from 'aws-amplify';
 // Get the aws resources configuration parameters
 import aws_exports from '../../aws-exports'; // if you are using Amplify CLI
 import { navigate } from 'gatsby';
 
 Amplify.configure(aws_exports);
+
+const authScreenLabels = {
+    en: {
+        'Sign Up': 'Create new account',
+        'Sign Up Account': 'Create a new account on Sho.Co',
+        'Sign in to your account': 'Sign in to your Sho.co account',
+        'Create a new account': 'Create a new Sho.Co account'
+    }
+};
+
+I18n.setLanguage('en');
+I18n.putVocabularies(authScreenLabels);
 
 // remember everything in /app/* is dynamic now!
 const App = () => {
@@ -31,4 +43,8 @@ function PublicRoute(props) {
   return <div>{props.children}</div>
 }
 
-export default withAuthenticator(App);
+const MyTheme = {
+  
+}
+
+export default withAuthenticator(App, true, [], null, MyTheme);;
