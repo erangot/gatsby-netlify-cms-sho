f0eb87e2fac58839b8c6dd9e1ab0810907b3a82d
Gelo Ilagan <gelo@ussi.com.ph>
2019-05-24 14:03:42 +0800

    fix errors on building

--- src/pages/app/component/PrivateRoute.js
+++ src/pages/app/component/PrivateRoute.js
@@ -4,18 +4,18 @@ import { navigate } from "gatsby"
 
 class PrivateRoute extends React.Component {
   componentDidMount = () => {
-    // const { location } = this.props
-    // if (!isLoggedIn() && location.pathname !== `/app/login`) {
-    //   // If the user is not logged in, redirect to the login page.
-    //   navigate(`/`)
-    //   return null
-    // }
+    const { location } = this.props
+    if (!isLoggedIn() && location.pathname !== `/app/login`) {
+      // If the user is not logged in, redirect to the login page.
+      navigate(`/`)
+      return null
+    }
   }
 
   render() {
     const { component: Component, location, ...rest } = this.props
-    // return isLoggedIn() ? <Component {...rest} /> : null
-    return <Component {...rest}/>
+    return isLoggedIn() ? <Component {...rest} /> : null
+    // return <Component {...rest}/>
   }
 }
 
